a4b41ed8ceb44df98203c761fa95faa13f15c979
Diego <diereyesh@gmail.com>
2018-04-18 14:45:22 -0300

    MEDIO

---
+++ Ejercicio.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+ 
+    int peso;
+ 
+    float estatura,imc;
+ 
+    cout<<"Introduce tu Peso (Kg):  ";
+ 
+    cin>>peso;
+ 
+    cout<<"Introduce tu Estatura  (Mts): ";
+ 
+    cin>>estatura;
+ 
+    imc=peso/(estatura*estatura);
+ 
+    cout<<"Tu IMC es de :   "<<imc<<endl;
+ 
+    return 0;
+}
